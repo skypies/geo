aff5a5cdf52256e56b11f2b293158a4938c4dbaa
Adam Worrall <adam@worrall.cc>
2015-12-18 16:21:08 -0800

    Minor reorg

--- latlong.go
+++ latlong.go
@@ -5,8 +5,6 @@ import(
 	"math"
 )
 
-// {{{ type Latlong
-
 type Latlong struct {
 	Lat, Long float64
 }
@@ -42,55 +40,6 @@ func (from Latlong)InterpolateTo(to Latlong, ratio float64) Latlong {
 		Long: interpFunc(from.Long, to.Long),
 	}
 }
-
-// }}}
-// {{{ type LatlongBox
-
-const (
-	kKmPerLatitudeDegreeAtSFO  = 111.2   // (36,-122)->(37,-122) == 111 KM (heading north)
-	kKmPerLongitudeDegreeAtSFO = 88.08   // (36,-122)->(36,-121) ==  88 KM (heading east)
-)
-
-type LatlongBox struct {
-	SW, NE Latlong
-}
-func (box LatlongBox)String() string { return fmt.Sprintf("%s-%s", box.SW, box.NE) }
-
-// Returns a box, centred on ll, that is of size (width,height)
-func (ll Latlong)Box(widthKm,heightKm float64) LatlongBox {
-	// This is a hack; these constants are only valid quite close to SFO itself.
-	latOffset := (heightKm / kKmPerLatitudeDegreeAtSFO) / 2.0
-	longOffset := (widthKm / kKmPerLongitudeDegreeAtSFO) / 2.0
-	return LatlongBox{
-		SW: Latlong{ll.Lat-latOffset, ll.Long-longOffset},
-		NE: Latlong{ll.Lat+latOffset, ll.Long+longOffset},
-	}
-}
-
-func (from Latlong)BoxTo(to Latlong) LatlongBox {
-	box := LatlongBox{}
-	if (from.Lat < to.Lat) {
-		box.SW.Lat, box.NE.Lat = from.Lat, to.Lat
-	} else {
-		box.SW.Lat, box.NE.Lat = to.Lat, from.Lat
-	}
-	if (from.Long < to.Long) {
-		box.SW.Long, box.NE.Long = from.Long, to.Long
-	} else {
-		box.SW.Long, box.NE.Long = to.Long, from.Long
-	}
-	return box
-}
-
-func (box LatlongBox)Contains(l Latlong) bool {
-	if (l.Long < box.SW.Long) { return false }
-	if (l.Lat  < box.SW.Lat ) { return false }
-	if (l.Long > box.NE.Long) { return false }
-	if (l.Lat  > box.NE.Lat ) { return false }
-	return true
-}
-
-// }}}
 
 // {{{ -------------------------={ E N D }=----------------------------------
 
---
+++ latlongbox.go
@@ -0,0 +1,58 @@
+package geo
+
+import(
+	"fmt"
+)
+
+const (
+	kKmPerLatitudeDegreeAtSFO  = 111.2   // (36,-122)->(37,-122) == 111 KM (heading north)
+	kKmPerLongitudeDegreeAtSFO = 88.08   // (36,-122)->(36,-121) ==  88 KM (heading east)
+)
+
+type LatlongBox struct {
+	SW, NE Latlong
+}
+func (box LatlongBox)String() string { return fmt.Sprintf("%s-%s", box.SW, box.NE) }
+
+
+// Returns a box, centred on ll, that is of size (width,height)
+func (ll Latlong)Box(widthKm,heightKm float64) LatlongBox {
+	// This is a hack; these constants are only valid quite close to SFO itself.
+	latOffset := (heightKm / kKmPerLatitudeDegreeAtSFO) / 2.0
+	longOffset := (widthKm / kKmPerLongitudeDegreeAtSFO) / 2.0
+	return LatlongBox{
+		SW: Latlong{ll.Lat-latOffset, ll.Long-longOffset},
+		NE: Latlong{ll.Lat+latOffset, ll.Long+longOffset},
+	}
+}
+
+func (from Latlong)BoxTo(to Latlong) LatlongBox {
+	box := LatlongBox{}
+	if (from.Lat < to.Lat) {
+		box.SW.Lat, box.NE.Lat = from.Lat, to.Lat
+	} else {
+		box.SW.Lat, box.NE.Lat = to.Lat, from.Lat
+	}
+	if (from.Long < to.Long) {
+		box.SW.Long, box.NE.Long = from.Long, to.Long
+	} else {
+		box.SW.Long, box.NE.Long = to.Long, from.Long
+	}
+	return box
+}
+
+func (box LatlongBox)Contains(l Latlong) bool {
+	if (l.Long < box.SW.Long) { return false }
+	if (l.Lat  < box.SW.Lat ) { return false }
+	if (l.Long > box.NE.Long) { return false }
+	if (l.Lat  > box.NE.Lat ) { return false }
+	return true
+}
+
+// {{{ -------------------------={ E N D }=----------------------------------
+
+// Local variables:
+// folded-file: t
+// end:
+
+// }}}
