a59124f44f0406fc2bf46ec0b371001309f3fae9
Adam Worrall <adam@worrall.cc>
2016-08-01 15:35:36 -0700

    Tidy up the interfaces

--- georestrictor.go
+++ georestrictor.go
@@ -1,24 +1,45 @@
 package geo
 
+//// https://play.golang.org/p/jF-SdC6Lxf
+
 // MapRenderer lets things output themselves as primitive shapes, to be rendered on a map
 type MapRenderer interface {
-	String() string	
+	String() string
 	ToLines() []LatlongLine
 	ToCircles() []LatlongCircle
 }
 
-// GeoRestricter means things that apply geo restrictions to tracks
-type GeoRestrictor interface {
-	String() string	
-
+// An Intersector thing implements standard intersection routines.
+type Intersector interface {
+	MapRenderer
 	IntersectsAltitude(int64) bool
 	IntersectsLine (LatlongLine) bool
 	IntersectsLineDeb (LatlongLine) (bool,string)
+	//IntersectsContiguousLineSet ([]LatlongLine) bool  // O(log N), plz
 	LookForExit() bool  // Should we enter, and then exit, this restrition ? Or is it a window ?
-
-	//IntersectsContiguousLineSet ([]LatlongLine) bool  // O(log N)
-
-	// Also implements MapRenderer
-	ToLines() []LatlongLine
-	ToCircles() []LatlongCircle
 }
+
+// A Restrictor thing implements intersection, and logic
+type Restrictor interface {
+	Intersector
+	MustNotIntersect() bool
+}
+
+// Wrapper objects, to turn geo objects into restrictors
+type LatlongBoxRestrictor struct {
+	LatlongBox
+	MustNotIntersectVal bool
+}
+func (r LatlongBoxRestrictor)MustNotIntersect() bool { return r.MustNotIntersectVal }
+
+type LatlongCircleRestrictor struct {
+	LatlongCircle
+	MustNotIntersectVal bool
+}
+func (r LatlongCircleRestrictor)MustNotIntersect() bool { return r.MustNotIntersectVal }
+
+type WindowRestrictor struct {
+	Window
+	MustNotIntersectVal bool
+}
+func (r WindowRestrictor)MustNotIntersect() bool { return r.MustNotIntersectVal }
